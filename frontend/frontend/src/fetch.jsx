c516b429fe8f2dd2836464f1f7c258aacdbabb74
Chris Zou <chzzou92@gmail.com>
2025-08-08 18:00:48 -0400

    railway fetch

--- frontend/frontend/src/fetch.jsx
+++ frontend/frontend/src/fetch.jsx
@@ -24,7 +24,7 @@ function SendData({ setFetchData, drivers, passengers, drawMultipleRoutes }) {
       setError(false);
       console.log(JSON.stringify(payload));
       const res = await fetch(
-        "http://localhost:8000/",
+        "https://routefinderdemo-production.up.railway.app/get-data",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
